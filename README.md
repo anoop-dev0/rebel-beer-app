061c6ed94b1f833464154fc07418218f7191f1fc
Anoop <anoop.living@gmail.com>
2020-11-28 22:19:34 +0530

    readme updated and search fixed

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# rebel-beer-app
+# Rebel-Foods Assignment
+
+This is a beer app
+
+## Installation
+
+clone this repository and install the dependencies : 
+ - Open this directory in terminal/command-prompt and execute following commands
+
+```bash
+npm install
+```
+```bash
+npm dev
+```
+ 
+app is running [rebel-foods-app](http://localhost:3000/).
+
+## Hosting
+
+Application is also being hosted on [heroku](https://rebel-foods-app.herokuapp.com/).
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {FormControl,InputGroup} from 'react-bootstrap';
 export default (props) => {
     return (
-        <InputGroup size="sm" className="mb-3">
+        <InputGroup size="sm" className="mb-3 mt-3">
             <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroup-sizing-sm">Small</InputGroup.Text>
+                <InputGroup.Text id="inputGroup-sizing-sm">Search by Brand</InputGroup.Text>
             </InputGroup.Prepend>
             <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" onKeyUp={props.filterList} />
         </InputGroup>
